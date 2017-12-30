32ae153a924a61f40e7dc4ec8a3691501bde6a16
Robert DeVore <deviodigital@gmail.com>
2017-12-29 19:44:06 -0500

    Added check for new 'Hide for Administrators' option

--- public/class-dispensary-age-verification-public.php
+++ public/class-dispensary-age-verification-public.php
@@ -115,6 +115,7 @@ class Dispensary_Age_Verification_Public {
 function wpd_ageverification() {
 
 ?>
+<?php if ( '' != get_theme_mod( 'dav_adminHide' ) && current_user_can( 'administrator' ) ) {} else { ?>
 <script type="text/javascript">
 	(function( $ ) {
 		$.ageCheck({
@@ -125,6 +126,7 @@ function wpd_ageverification() {
     });
 	})( jQuery );
 </script>
+<?php } ?>
 <?php
 }
 add_action( 'wp_footer', 'wpd_ageverification' );
