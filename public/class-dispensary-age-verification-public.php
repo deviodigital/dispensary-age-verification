0c9ccba5f7ab46b8994077a0fdfeb9aac476aa6e
Robert DeVore <deviodigital@gmail.com>
2019-07-09 20:53:28 -0400

    :bug: BUG: Fixed background image CSS

--- public/class-dispensary-age-verification-public.php
+++ public/class-dispensary-age-verification-public.php
@@ -138,7 +138,7 @@ function wpd_av_public_css() {
 	if ( '' !== get_theme_mod( 'dav_bgImage' ) ) { ?>
 		<style type="text/css">
 		.wpd-av-overlay {
-			background: url(<?php echo get_theme_mod('dav_bgImage'); ?>) no-repeat center center;
+			background-image: url(<?php echo get_theme_mod('dav_bgImage'); ?>) no-repeat center center;
 			box-sizing: border-box;
 			background-size: cover;
 			background-attachment: fixed;
