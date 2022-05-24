5ea288aa8a20d664595b64c6e7980481b882cb55
Robert DeVore <deviodigital@gmail.com>
2022-05-23 22:41:50 -0400

    :ok_hand: IMPROVE: WordPress Coding Standards

--- uninstall.php
+++ uninstall.php
@@ -10,18 +10,20 @@
  * - Run an admin referrer check to make sure it goes through authentication
  * - Verify the output of $_GET makes sense
  * - Repeat with other user roles. Best directly by using the links/query string parameters.
- * - Repeat things for multisite. Once for a single site in the network, once sitewide.
+ * - Repeat things for multisite. Once for a single site, once sitewide.
  *
- * This file may be updated more in future version of the Boilerplate; however, this is the
- * general skeleton and outline for how the file should work.
+ * This file may be updated more in future version of the Boilerplate; however, this
+ * is the general skeleton and outline for how the file should work.
  *
  * For more information, see the following discussion:
  * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
  *
- * @link       https://www.deviodigital.com
- * @since      1.0.0
- *
- * @package    Age_Verification
+ * @package Age_Verification
+ * 
+ * @author  Devio Digital <contact@deviodigital.com>
+ * @license GPL-2.0+ http://www.gnu.org/licenses/gpl-2.0.txt
+ * @link    https://www.deviodigital.com
+ * @since   1.0.0
  */
 
 // If uninstall not called from WordPress, then exit.
